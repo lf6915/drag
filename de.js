1f0f8165424ba476d22d6334fc99f34ff5dac74a
lf6915 <lf6915@163.com>
2016-09-07 22:48:05 +0800

    revise two fails

--- de.js
+++ de.js
@@ -1,3 +1,29 @@
 /**
  * Created by LF on 2016/9/7.
  */
+window.onload=function()
+{
+    var oDiv=document.getElementById('box');
+    oDiv.onmousedown=function(ev)
+    {
+        var oEvent=ev||event;
+        var disX=oEvent.clientX-oDiv.offsetLeft;
+        var disY=oEvent.clientY-oDiv.offsetTop;
+        document.onmouseover=function(ev)
+        {
+            var oEvent=ev||event;
+            var left=oEvent.clientX-disX;
+            var top=oEvent.clientY-disY;
+            oDiv.style.left=left+'px';
+            oDiv.style.top=top+'px';
+        };
+        document.onmouseup=function()
+        {
+            document.onmousemove=null;
+            document.onmouseup=null;
+            oDiv.releaseCapture&&oDiv.releaseCapture();
+        };
+        oDiv.setCapture&&oDiv.setCapture();
+        return false;
+    };
+};
